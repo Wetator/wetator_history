732bfce65ca69a73a253cc61da4c6cdbd08c90cb
rbri <rbri@localhost>
2010-05-28 18:48:34 +0000

    Version 0.9.1

--- src/org/rbri/wet/Version.java
+++ src/org/rbri/wet/Version.java
@@ -26,7 +26,7 @@ public class Version {
 
     public static final String PRODUCT_NAME = "Wetator";
     public static final String VERSION = "0.9.1";
-    public static final String BUILD = "2010042506";
+    public static final String BUILD = "2010052801";
 
     public static void main(String[] anArgsArray) {
         System.out.println(getFullProductName());
