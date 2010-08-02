4d6b91ebb5e918933077907932fc9b70acdd5b4a
rbri <rbri@localhost>
2010-08-02 19:30:40 +0000

    javadoc

--- src/org/rbri/wet/util/StdOutProgressListener.java
+++ src/org/rbri/wet/util/StdOutProgressListener.java
@@ -42,6 +42,11 @@ public class StdOutProgressListener implements WetEngineProgressListener {
   private int dotCount;
   private int contextDeep;
 
+  /**
+   * {@inheritDoc}
+   * 
+   * @see org.rbri.wet.core.WetEngineProgressListener#engineSetup(org.rbri.wet.core.WetEngine)
+   */
   public void engineSetup(WetEngine aWetEngine) {
     println(Version.getProductName() + " " + Version.getVersion());
     println("  using " + com.gargoylesoftware.htmlunit.Version.getProductName() + " version "
@@ -70,13 +75,29 @@ public class StdOutProgressListener implements WetEngineProgressListener {
     }
   }
 
+  /**
+   * {@inheritDoc}
+   * 
+   * @see org.rbri.wet.core.WetEngineProgressListener#contextExecuteCommandStart(org.rbri.wet.core.WetContext,
+   *      org.rbri.wet.core.WetCommand)
+   */
   public void contextExecuteCommandStart(WetContext aWetContext, WetCommand aWommand) {
     stepsCount++;
   }
 
+  /**
+   * {@inheritDoc}
+   * 
+   * @see org.rbri.wet.core.WetEngineProgressListener#contextExecuteCommandEnd()
+   */
   public void contextExecuteCommandEnd() {
   }
 
+  /**
+   * {@inheritDoc}
+   * 
+   * @see org.rbri.wet.core.WetEngineProgressListener#contextExecuteCommandError(java.lang.Throwable)
+   */
   public void contextExecuteCommandError(Throwable aThrowable) {
     errorCount++;
 
@@ -89,6 +110,11 @@ public class StdOutProgressListener implements WetEngineProgressListener {
     dotCount++;
   }
 
+  /**
+   * {@inheritDoc}
+   * 
+   * @see org.rbri.wet.core.WetEngineProgressListener#contextExecuteCommandFailure(org.rbri.wet.exception.AssertionFailedException)
+   */
   public void contextExecuteCommandFailure(AssertionFailedException anAssertionFailedException) {
     failureCount++;
 
@@ -101,6 +127,11 @@ public class StdOutProgressListener implements WetEngineProgressListener {
     dotCount++;
   }
 
+  /**
+   * {@inheritDoc}
+   * 
+   * @see org.rbri.wet.core.WetEngineProgressListener#contextExecuteCommandSuccess()
+   */
   public void contextExecuteCommandSuccess() {
     if (dotCount == DOTS_PER_LINE) {
       println(".");
@@ -111,6 +142,11 @@ public class StdOutProgressListener implements WetEngineProgressListener {
     dotCount++;
   }
 
+  /**
+   * {@inheritDoc}
+   * 
+   * @see org.rbri.wet.core.WetEngineProgressListener#contextTestEnd()
+   */
   public void contextTestEnd() {
     contextDeep--;
     if (contextDeep > 0) {
@@ -121,6 +157,11 @@ public class StdOutProgressListener implements WetEngineProgressListener {
     }
   }
 
+  /**
+   * {@inheritDoc}
+   * 
+   * @see org.rbri.wet.core.WetEngineProgressListener#contextTestStart(java.lang.String, java.lang.String)
+   */
   public void contextTestStart(String aFileName, String aBrowserName) {
     if (contextDeep > 0) {
       // subcontext
@@ -132,34 +173,79 @@ public class StdOutProgressListener implements WetEngineProgressListener {
     contextDeep++;
   }
 
+  /**
+   * {@inheritDoc}
+   * 
+   * @see org.rbri.wet.core.WetEngineProgressListener#engineTestStart()
+   */
   public void engineTestStart() {
   }
 
+  /**
+   * {@inheritDoc}
+   * 
+   * @see org.rbri.wet.core.WetEngineProgressListener#engineResponseStored(java.lang.String)
+   */
   public void engineResponseStored(String aResponseFileName) {
   }
 
+  /**
+   * {@inheritDoc}
+   * 
+   * @see org.rbri.wet.core.WetEngineProgressListener#engineTestEnd()
+   */
   public void engineTestEnd() {
   }
 
+  /**
+   * {@inheritDoc}
+   * 
+   * @see org.rbri.wet.core.WetEngineProgressListener#engineFinish()
+   */
   public void engineFinish() {
     // print summary
     println("");
     println("Steps: " + stepsCount + ",  Failures: " + failureCount + ",  Errors: " + errorCount);
   }
 
+  /**
+   * {@inheritDoc}
+   * 
+   * @see org.rbri.wet.core.WetEngineProgressListener#commandSetSetup(org.rbri.wet.commandset.WetCommandSet)
+   */
   public void commandSetSetup(WetCommandSet aWetCommandSet) {
   }
 
+  /**
+   * {@inheritDoc}
+   * 
+   * @see org.rbri.wet.core.WetEngineProgressListener#warn(java.lang.String, java.lang.String[])
+   */
   public void warn(String aMessageKey, String[] aParameterArray) {
   }
 
+  /**
+   * {@inheritDoc}
+   * 
+   * @see org.rbri.wet.core.WetEngineProgressListener#info(java.lang.String, java.lang.String[])
+   */
   public void info(String aMessageKey, String[] aParameterArray) {
   }
 
+  /**
+   * The worker that does the real output
+   * 
+   * @param aString the output
+   */
   protected void println(String aString) {
     System.out.println(aString);
   }
 
+  /**
+   * The worker that does the real output
+   * 
+   * @param aString the output
+   */
   protected void print(String aString) {
     System.out.print(aString);
   }
