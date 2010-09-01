c97710aa22705146a1e67c1f5136d699ae8b6464
rbri <rbri@localhost>
2010-09-01 06:19:42 +0000

    next beta

--- src/org/rbri/wet/Version.java
+++ src/org/rbri/wet/Version.java
@@ -30,7 +30,7 @@ public final class Version {
   /**
    * The version.
    */
-  public static final String VERSION = "0.9.3 beta1";
+  public static final String VERSION = "0.9.3 beta2";
   /**
    * The build.
    */
