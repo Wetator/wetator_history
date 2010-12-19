b944a3daac22c62dc80df75ee1cb2a00ab088b64
rbri <rbri@localhost>
2010-12-19 12:56:47 +0000

    one more test

--- test/java/org/wetator/backend/htmlunit/control/HtmlUnitBaseControlTest.java
+++ test/java/org/wetator/backend/htmlunit/control/HtmlUnitBaseControlTest.java
@@ -25,6 +25,7 @@ import org.wetator.exception.AssertionFailedException;
 
 import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
 
 /**
  * @author rbri
@@ -80,13 +81,26 @@ public class HtmlUnitBaseControlTest {
   }
 
   @Test
-  public void isDisabled_ReadOnly() throws IOException, AssertionFailedException {
+  public void isDisabledGeneric_ReadOnly() throws IOException, AssertionFailedException {
     String tmpHtmlCode = "<html><body>" + "<form action='test'>"
         + "<input type='text' id='myId' name='MyName' value='value' readonly='readonly'/>" + "</form>"
         + "</body></html>";
     HtmlPage tmpHtmlPage = PageUtil.constructHtmlPage(tmpHtmlCode);
 
     HtmlUnitBaseControl<?> tmpControl = new HtmlUnitBaseControl<HtmlElement>(tmpHtmlPage.getElementById("myId"));
+
+    Assert.assertTrue(tmpControl.isDisabled(null));
+  }
+
+  @Test
+  public void isDisabled_ReadOnly() throws IOException, AssertionFailedException {
+    String tmpHtmlCode = "<html><body>" + "<form action='test'>"
+        + "<input type='text' id='myId' name='MyName' value='value' readonly='readonly'/>" + "</form>"
+        + "</body></html>";
+    HtmlPage tmpHtmlPage = PageUtil.constructHtmlPage(tmpHtmlCode);
+
+    HtmlTextInput tmpTextInput = (HtmlTextInput) tmpHtmlPage.getElementById("myId");
+    HtmlUnitBaseControl<?> tmpControl = new HtmlUnitInputText(tmpTextInput);
 
     Assert.assertTrue(tmpControl.isDisabled(null));
   }
