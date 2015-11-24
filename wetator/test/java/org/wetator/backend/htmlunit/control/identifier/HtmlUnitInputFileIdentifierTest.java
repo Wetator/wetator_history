5bd035147b440130c14940492e43b489483d4be8
rbri <rbri@localhost>
2015-11-24 18:33:54 +0000

    + release 1.3

---
+++ wetator/test/java/org/wetator/backend/htmlunit/control/identifier/HtmlUnitInputFileIdentifierTest.java
@@ -0,0 +1,174 @@
+/*
+ * Copyright (c) 2008-2015 wetator.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package org.wetator.backend.htmlunit.control.identifier;
+
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.wetator.backend.WPath;
+import org.wetator.backend.WeightedControlList;
+import org.wetator.exception.InvalidInputException;
+import org.wetator.util.SecretString;
+
+/**
+ * @author rbri
+ * @author frank.danek
+ */
+public class HtmlUnitInputFileIdentifierTest extends AbstractHtmlUnitControlIdentifierTest {
+
+  @Before
+  public void setupIdentifier() {
+    identifier = new HtmlUnitInputFileIdentifier();
+  }
+
+  @Test
+  public void byId() throws IOException, InvalidInputException {
+    // @formatter:off
+    final String tmpHtmlCode = "<html><body>"
+        + "<form action='test'>"
+        + "<input id='myId' name='myName' type='file'>"
+        + "</form>"
+        + "</body></html>";
+    // @formatter:on
+
+    final SecretString tmpSearch = new SecretString("myId");
+
+    final WeightedControlList tmpFound = identify(tmpHtmlCode, new WPath(tmpSearch, config), "myId");
+
+    Assert.assertEquals(1, tmpFound.getEntriesSorted().size());
+
+    Assert.assertEquals(
+        "[HtmlFileInput (id='myId') (name='myName')] found by: BY_ID coverage: 0 distance: 0 start: 0 index: 5",
+        tmpFound.getEntriesSorted().get(0).toString());
+  }
+
+  @Test
+  public void byName() throws IOException, InvalidInputException {
+    // @formatter:off
+    final String tmpHtmlCode = "<html><body>"
+        + "<form action='test'>"
+        + "<input id='myId' name='myName' type='file'>"
+        + "</form>"
+        + "</body></html>";
+    // @formatter:on
+
+    final SecretString tmpSearch = new SecretString("myName");
+
+    final WeightedControlList tmpFound = identify(tmpHtmlCode, new WPath(tmpSearch, config), "myId");
+
+    Assert.assertEquals(1, tmpFound.getEntriesSorted().size());
+
+    Assert.assertEquals(
+        "[HtmlFileInput (id='myId') (name='myName')] found by: BY_NAME coverage: 0 distance: 0 start: 0 index: 5",
+        tmpFound.getEntriesSorted().get(0).toString());
+  }
+
+  @Test
+  public void byLabelTextBefore() throws IOException, InvalidInputException {
+    // @formatter:off
+    final String tmpHtmlCode = "<html><body>"
+        + "<form action='test'>"
+        + "<input id='otherId' name='otherName' type='file'>"
+        + "<p>Marker</p>"
+        + "<input id='myId' name='myName' type='file'>"
+        + "</form>"
+        + "</body></html>";
+    // @formatter:on
+
+    final SecretString tmpSearch = new SecretString("Marker");
+
+    final WeightedControlList tmpFound = identify(tmpHtmlCode, new WPath(tmpSearch, config), "otherId", "myId");
+
+    Assert.assertEquals(1, tmpFound.getEntriesSorted().size());
+
+    Assert
+    .assertEquals(
+        "[HtmlFileInput (id='myId') (name='myName')] found by: BY_LABEL_TEXT coverage: 0 distance: 0 start: 6 index: 8",
+        tmpFound.getEntriesSorted().get(0).toString());
+  }
+
+  @Test
+  public void byWholeTextBefore() throws IOException, InvalidInputException {
+    // @formatter:off
+    final String tmpHtmlCode = "<html><body>"
+        + "<form action='test'>"
+        + "<p>Marker1</p>"
+        + "<input id='otherId' name='otherName' type='checkbox'>"
+        + "<p>Marker2</p>"
+        + "<input id='myId' name='myName' type='file'>"
+        + "</form>"
+        + "</body></html>";
+    // @formatter:on
+
+    final SecretString tmpSearch = new SecretString("Marker1");
+
+    final WeightedControlList tmpFound = identify(tmpHtmlCode, new WPath(tmpSearch, config), "otherId", "myId");
+
+    Assert.assertEquals(1, tmpFound.getEntriesSorted().size());
+
+    Assert.assertEquals(
+        "[HtmlFileInput (id='myId') (name='myName')] found by: BY_TEXT coverage: 8 distance: 15 start: 15 index: 10",
+        tmpFound.getEntriesSorted().get(0).toString());
+  }
+
+  @Test
+  public void byHtmlLabel_Text() throws IOException, InvalidInputException {
+    // @formatter:off
+    final String tmpHtmlCode = "<html><body>"
+        + "<form action='test'>"
+        + "<label id='labelId' for='myId'>Label</label>"
+        + "<input id='myId' name='myName' type='file'>"
+        + "</form>"
+        + "</body></html>";
+    // @formatter:on
+
+    final SecretString tmpSearch = new SecretString("Label");
+
+    final WeightedControlList tmpFound = identify(tmpHtmlCode, new WPath(tmpSearch, config), "labelId");
+
+    Assert.assertEquals(1, tmpFound.getEntriesSorted().size());
+
+    Assert.assertEquals(
+        "[HtmlFileInput (id='myId') (name='myName')] found by: BY_LABEL coverage: 0 distance: 0 start: 5 index: 7",
+        tmpFound.getEntriesSorted().get(0).toString());
+  }
+
+  @Test
+  public void byHtmlLabelChild_Text() throws IOException, InvalidInputException {
+    // @formatter:off
+    final String tmpHtmlCode = "<html><body>"
+        + "<form action='test'>"
+        + "<label id='labelId'>Label"
+        + "<input id='myId' name='myName' type='file'>"
+        + "</label>"
+        + "</form>"
+        + "</body></html>";
+    // @formatter:on
+
+    final SecretString tmpSearch = new SecretString("Label");
+
+    final WeightedControlList tmpFound = identify(tmpHtmlCode, new WPath(tmpSearch, config), "labelId");
+
+    Assert.assertEquals(1, tmpFound.getEntriesSorted().size());
+
+    Assert.assertEquals(
+        "[HtmlFileInput (id='myId') (name='myName')] found by: BY_LABEL coverage: 0 distance: 0 start: 5 index: 7",
+        tmpFound.getEntriesSorted().get(0).toString());
+  }
+}
---
+++ wetator/xsl/resources/wetator_report.js
@@ -0,0 +1,182 @@
+/*
+ * Copyright (c) 2008-2015 wetator.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+/* JS functions used by the Wetator report */
+
+
+    function showOrHideAll(image, imageId, id) {
+        var tmpSearch = "expandall.png";
+        if (toggleImage(image)) {
+            tmpSearch = "collapseall.png";
+        }
+        for (var i=1; i<arguments.length; i=i+2) {
+            var tmpImage=document.getElementById(arguments[i]);
+            if (tmpImage && tmpImage.src.indexOf(tmpSearch) != -1) {
+                showOrHide(tmpImage, arguments[i+1]);
+            }
+        }
+    }
+
+    function showOrHide(image, id) {
+        var tmpElement=document.getElementById(id);
+        if (toggleImage(image)) {
+            tmpElement.style.display = "none";
+        } else {
+            tmpElement.style.display = "";
+        }
+    }
+
+    function toggleImage(image) {
+        if (image.src.indexOf("collapseall.png") != -1) {
+            image.src = image.src.substr(0, image.src.lastIndexOf("/")+1)+"expandall.png";
+            return true;
+        }
+        if (image.src.indexOf("expandall.png") != -1) {
+            image.src = image.src.substr(0, image.src.lastIndexOf("/")+1)+"collapseall.png";
+            return false;
+        }
+        if (image.src.indexOf("collapselog.png") != -1) {
+            image.src = image.src.substr(0, image.src.lastIndexOf("/")+1)+"expandlog.png";
+            return true;
+        }
+        if (image.src.indexOf("expandlog.png") != -1) {
+            image.src = image.src.substr(0, image.src.lastIndexOf("/")+1)+"collapselog.png";
+            return false;
+        }
+        if (image.src.indexOf("collapselogwarn.png") != -1) {
+            image.src = image.src.substr(0, image.src.lastIndexOf("/")+1)+"expandlogwarn.png";
+            return true;
+        }
+        if (image.src.indexOf("expandlogwarn.png") != -1) {
+            image.src = image.src.substr(0, image.src.lastIndexOf("/")+1)+"collapselogwarn.png";
+            return false;
+        }
+    }
+
+    function makeVisible(id) {
+        var tmpImage = document.getElementById('showHide_' + id);
+        if (tmpImage && tmpImage.src.indexOf("expandall.png") != -1) {
+            showOrHide(tmpImage, id);
+        }
+    }
+
+    function switchTables(tableToHide, tableToShowId, force) {
+        var overviewSwitcher=document.getElementById('overviewswitcher');
+        if (force || overviewSwitcher.src.indexOf("expandall.png") != -1) {
+            tableToHide.style.display = "none";
+            document.getElementById(tableToShowId).style.display = "";
+        }
+    }
+
+    function switchOverviewTables(image) {
+        if (image.src.indexOf("expandall.png") != -1) {
+            var tmpDetailedOverview=document.getElementById('detailedoverview');
+            switchTables(tmpDetailedOverview, 'summaryoverview', true);
+        } else if (image.src.indexOf("collapseall.png") != -1) {
+            var tmpSummaryOverview=document.getElementById('summaryoverview');
+            switchTables(tmpSummaryOverview, 'detailedoverview', true);
+        }
+    }
+
+    function showAllTestCases() {
+    	$('.successful').removeClass('hidden');
+    	$('#hideSuccessful').removeClass('hidden');
+    	$('#showAll').addClass('hidden');
+    }
+    
+    function hideSuccessfulTestCases() {
+    	$('.successful').addClass('hidden');
+    	$('#hideSuccessful').addClass('hidden');
+    	$('#showAll').removeClass('hidden');
+    }
+
+    var wetFrameHeight = -1;
+
+    function showPreview(e, src) {
+        // preview only for html and txt files
+        if ((src.lastIndexOf(".html") != src.length - 5)
+            && (src.indexOf(".html?highlight=") < 0)
+            && (src.indexOf(".txt") != src.length - 4)) {
+            return;
+        }
+
+        var tmpFrame = document.getElementById('preview');
+        tmpFrame.contentWindow.location.replace(src);
+        tmpFrame.onload = function() {
+
+            if (wetFrameHeight < 0) {
+                tmpFrame.style.display = 'block';
+                wetFrameHeight = tmpFrame.getBoundingClientRect().height;
+                wetFrameHeight += 30;
+            }
+
+            var box = e.target.getBoundingClientRect();
+            var tmpPosX = box.right + 20;
+            var tmpPosY = Math.max(Math.min(box.top, window.innerHeight - wetFrameHeight), 10);
+
+            if(window.pageYOffset) {
+                tmpPosX = tmpPosX + window.pageXOffset;
+                tmpPosY = tmpPosY + window.pageYOffset;
+            } else {
+                tmpPosX = tmpPosX + document.body.scrollLeft;
+                tmpPosY = tmpPosY + document.body.scrollTop;
+            }
+
+            tmpFrame.style.left = tmpPosX + "px";
+            tmpFrame.style.top = tmpPosY + "px";
+            tmpFrame.style.display='block';
+            tmpFrame.contentWindow.document.onmouseout=hidePreview;
+            tmpFrame.contentWindow.document.onmouseover=hidePreview;
+
+            tmpFrame.contentWindow.highlight();
+        }
+    }
+
+    function hidePreview() {
+        var tmpFrame = document.getElementById('preview');
+        tmpFrame.style.display='none';
+    }
+
+    function highlight() {
+        setTimeout( function() {
+            var win = window;
+            var sel = getParameterByName(win, 'highlight');
+            while ((!sel || sel.length === 0) && (win.parent && win.parent != win)) {
+                win = win.parent;
+                sel = getParameterByName(win, 'highlight');
+            }
+            if (!sel || sel.length === 0) { return; }
+  
+            var elem = $(sel);
+            if (!elem) { return; }
+
+            elem.css('box-shadow', '0 0 2px 2px #E65212');
+            var offset = elem.offset();
+            offset.left -= 200;
+            offset.top -= 400;
+            $('html, body').animate({
+                scrollTop: offset.top,
+                scrollLeft: offset.left
+            })
+        }, 44);
+    }
+
+    function getParameterByName(win, name) {
+        name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+        var regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
+        var results = regex.exec(win.location.search);
+        return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+    }
