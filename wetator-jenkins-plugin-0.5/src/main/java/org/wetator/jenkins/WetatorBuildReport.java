27eb58ed4149af5d6069fb37aa9d0c5fa761bbd0
frank.danek <frank.danek@localhost>
2016-01-25 20:59:00 +0000

    [maven-release-plugin] copy for tag wetator-jenkins-plugin-0.5

---
+++ wetator-jenkins-plugin-0.5/src/main/java/org/wetator/jenkins/WetatorBuildReport.java
@@ -0,0 +1,364 @@
+/*
+ * Copyright (c) 2008-2016 wetator.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package org.wetator.jenkins;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+import java.lang.ref.WeakReference;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.jvnet.localizer.Localizable;
+import org.kohsuke.stapler.StaplerProxy;
+import org.kohsuke.stapler.StaplerRequest;
+import org.kohsuke.stapler.StaplerResponse;
+import org.kohsuke.stapler.export.ExportedBean;
+import org.wetator.jenkins.result.AbstractBaseResult;
+import org.wetator.jenkins.result.BrowserResult;
+import org.wetator.jenkins.result.StepError;
+import org.wetator.jenkins.result.TestError;
+import org.wetator.jenkins.result.TestFileResult;
+import org.wetator.jenkins.result.TestResult;
+import org.wetator.jenkins.result.TestResults;
+import org.wetator.jenkins.util.ErrorConverter;
+import org.wetator.jenkins.util.GZIPXMLFile;
+
+import com.thoughtworks.xstream.XStream;
+
+import hudson.Functions;
+import hudson.model.AbstractBuild;
+import hudson.model.BuildListener;
+import hudson.model.HealthReport;
+import hudson.model.HealthReportingAction;
+import hudson.tasks.test.TestObject;
+import hudson.util.HeapSpaceStringConverter;
+import hudson.util.XStream2;
+
+/**
+ * The Wetator Report for one build.
+ *
+ * @author frank.danek
+ */
+@ExportedBean
+public class WetatorBuildReport implements HealthReportingAction, StaplerProxy, Serializable {
+
+  private static final long serialVersionUID = -7087201387669424587L;
+
+  private static final Logger LOG = Logger.getLogger(WetatorBuildReport.class.getName());
+
+  public final AbstractBuild<?, ?> build;
+
+  private transient WeakReference<TestResults> results;
+  private Integer failCount;
+  private Integer skipCount;
+  private Integer totalCount;
+
+  private Map<String, String> descriptions = new ConcurrentHashMap<>();
+
+  /**
+   * The constructor.
+   *
+   * @param build the build this report belongs to
+   * @param results the {@link TestResults} to report
+   */
+  public WetatorBuildReport(AbstractBuild<?, ?> build, TestResults results, BuildListener listener) {
+    // the method parameters must be raw (without leading a) to make stapler work
+    this.build = build;
+    setResults(results, listener);
+  }
+
+  @Override
+  public String getIconFileName() {
+    return PluginImpl.ICON_FILE_NAME;
+  }
+
+  @Override
+  public String getDisplayName() {
+    return Messages.WetatorBuildReport_DisplayName();
+  }
+
+  @Override
+  public String getUrlName() {
+    return PluginImpl.URL_NAME;
+  }
+
+  /**
+   * @return the build this report belongs to
+   */
+  public AbstractBuild<?, ?> getBuild() {
+    return build;
+  }
+
+  /**
+   * @return the {@link TestResults}
+   */
+  public synchronized TestResults getResults() {
+    TestResults tmpResults;
+    if (results == null) {
+      tmpResults = load();
+      results = new WeakReference<TestResults>(tmpResults);
+    } else {
+      tmpResults = results.get();
+    }
+
+    if (tmpResults == null) {
+      tmpResults = load();
+      results = new WeakReference<TestResults>(tmpResults);
+    }
+    if (totalCount == null) {
+      totalCount = Integer.valueOf(tmpResults.getTotalCount());
+      failCount = Integer.valueOf(tmpResults.getFailCount());
+      skipCount = Integer.valueOf(tmpResults.getSkipCount());
+    }
+    return tmpResults;
+  }
+
+  /**
+   * Overwrites the {@link TestResults} by a new data set.
+   *
+   * @param aResults the results to set
+   * @param aListener the current {@link BuildListener}
+   */
+  public synchronized void setResults(TestResults aResults, BuildListener aListener) {
+    aResults.tally();
+    aResults.setOwner(build);
+
+    totalCount = Integer.valueOf(aResults.getTotalCount());
+    failCount = Integer.valueOf(aResults.getFailCount());
+    skipCount = Integer.valueOf(aResults.getSkipCount());
+
+    // persist the data
+    try {
+      getDataFile().write(aResults);
+    } catch (IOException e) {
+      e.printStackTrace(aListener.fatalError("Failed to save the Wetator test result"));
+    }
+
+    results = new WeakReference<TestResults>(aResults);
+  }
+
+  /**
+   * Convenience method to access the totalCount.
+   *
+   * @return the totalCount
+   */
+  public int getTotalCount() {
+    if (totalCount == null) {
+      // this will compute the result
+      getResults();
+    }
+    if (totalCount == null) {
+      return 0;
+    }
+    return totalCount.intValue();
+  }
+
+  /**
+   * Convenience method to access the failCount.
+   *
+   * @return the failCount
+   */
+  public int getFailCount() {
+    if (failCount == null) {
+      // this will compute the result
+      getResults();
+    }
+    if (failCount == null) {
+      return 0;
+    }
+    return failCount.intValue();
+  }
+
+  /**
+   * Convenience method to access the skipCount.
+   *
+   * @return the skipCount
+   */
+  public int getSkipCount() {
+    if (skipCount == null) {
+      // this will compute the result
+      getResults();
+    }
+    if (skipCount == null) {
+      return 0;
+    }
+    return skipCount.intValue();
+  }
+
+  /**
+   * {@link AbstractBaseResult}s do not have their own persistence mechanism, so updatable data of
+   * {@link AbstractBaseResult}s need to be persisted by the owning {@link WetatorBuildReport}, and this method and
+   * {@link #setDescription(AbstractBaseResult, String)} provide that logic.<br>
+   * <br>
+   * The default implementation stores information in the 'this' object.
+   *
+   * @see TestObject#getDescription()
+   */
+  public String getDescription(AbstractBaseResult aResult) {
+    return descriptions.get(aResult.getName());
+  }
+
+  /**
+   * @param aResult the result to set the description for
+   * @param aDescription the description to set
+   */
+  public void setDescription(AbstractBaseResult aResult, String aDescription) {
+    descriptions.put(aResult.getName(), aDescription);
+  }
+
+  /**
+   * Used for deserialization (magic java method name).
+   *
+   * @return this
+   */
+  public Object readResolve() {
+    if (descriptions == null) {
+      descriptions = new ConcurrentHashMap<>();
+    }
+    return this;
+  }
+
+  /**
+   * Gets the diff string of failures.<br>
+   * (for resultSummary.jelly)
+   */
+  public final String getFailureDiffString() {
+    WetatorBuildReport tmpPrevious = getPreviousReport();
+    if (tmpPrevious == null) {
+      return ""; // no record
+    }
+
+    int tmpDiff = getFailCount() - tmpPrevious.getFailCount();
+    return " " + Messages.WetatorBuildReport_FailureDiff(Functions.getDiffString(tmpDiff));
+  }
+
+  @Override
+  public HealthReport getBuildHealth() {
+    final int tmpTotalCount = getTotalCount();
+    final int tmpFailCount = getFailCount();
+    final int tmpSkipCount = getSkipCount();
+    int tmpScore = 100;
+    if (tmpTotalCount != 0) {
+      tmpScore = (int) (100.0 * (1.0 - ((double) tmpFailCount + tmpSkipCount) / tmpTotalCount));
+    }
+    Localizable tmpDisplayName = Messages._WetatorBuildReport_DisplayName();
+    Localizable tmpDescription;
+    if (tmpTotalCount == 0) {
+      tmpDescription = Messages._WetatorBuildReport_zeroTestDescription(tmpDisplayName);
+    } else {
+      tmpDescription = Messages._WetatorBuildReport_TestsDescription(tmpDisplayName, tmpFailCount, tmpTotalCount,
+          tmpSkipCount);
+    }
+    return new HealthReport(tmpScore, tmpDescription);
+  }
+
+  /**
+   * @return the previous result
+   */
+  public TestResults getPreviousResult() {
+    WetatorBuildReport tmpPrevious = getPreviousReport();
+    if (tmpPrevious == null) {
+      return null;
+    }
+    return tmpPrevious.getResults();
+  }
+
+  /**
+   * Finds the result for the given name.
+   *
+   * @param aName the name of the result
+   * @return the result
+   */
+  public AbstractBaseResult findCorrespondingResult(String aName) {
+    return getResults().findCorrespondingResult(aName);
+  }
+
+  private WetatorBuildReport getPreviousReport() {
+    AbstractBuild<?, ?> tmpBuild = build;
+    while (true) {
+      tmpBuild = tmpBuild.getPreviousBuild();
+      if (tmpBuild == null) {
+        return null;
+      }
+      WetatorBuildReport tmpReport = tmpBuild.getAction(WetatorBuildReport.class);
+      if (tmpReport != null) {
+        return tmpReport;
+      }
+    }
+  }
+
+  private GZIPXMLFile getDataFile() {
+    return new GZIPXMLFile(XSTREAM, new File(build.getRootDir(), PluginImpl.TEST_RESULTS_FILE_NAME));
+  }
+
+  /**
+   * Loads a {@link TestResults} from disk.
+   *
+   * @return the loaded {@link TestResults}
+   */
+  private TestResults load() {
+    TestResults tmpTestResults;
+    try {
+      tmpTestResults = (TestResults) getDataFile().read();
+    } catch (IOException e) {
+      LOG.log(Level.WARNING, "Failed to load " + getDataFile());
+      tmpTestResults = new TestResults("dummy"); // return a dummy
+    }
+    tmpTestResults.tally();
+    tmpTestResults.setOwner(build);
+    return tmpTestResults;
+  }
+
+  private static final XStream XSTREAM = new XStream2();
+
+  static {
+    initializeXStream(XSTREAM);
+  }
+
+  public static void initializeXStream(XStream anXStream) {
+    anXStream.alias("testResults", TestResults.class);
+    anXStream.alias("testResult", TestResult.class);
+    anXStream.alias("testFileResult", TestFileResult.class);
+    anXStream.alias("browserResult", BrowserResult.class);
+    anXStream.alias("stepError", StepError.class);
+    anXStream.alias("testError", TestError.class);
+    anXStream.registerConverter(new ErrorConverter(anXStream.getMapper(),
+        anXStream.getConverterLookup().lookupConverterForType(TestError.class), anXStream.getReflectionProvider()));
+    anXStream.registerConverter(new HeapSpaceStringConverter(), 100);
+  }
+
+  @Override
+  public Object getTarget() {
+    return getResults();
+  }
+
+  /**
+   * Used by stapler.
+   *
+   * @param token the token to get
+   * @param request the request
+   * @param response the response
+   * @return this if the token matches or null
+   */
+  public Object getDynamic(String token, StaplerRequest request, StaplerResponse response) {
+    // the method parameters must be raw (without leading a) to make stapler work
+    return getResults().getDynamic(token, request, response);
+  }
+}
---
+++ wetator-jenkins-plugin-0.5/src/main/java/org/wetator/jenkins/result/AbstractBaseResult.java
@@ -0,0 +1,279 @@
+/*
+ * Copyright (c) 2008-2016 wetator.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package org.wetator.jenkins.result;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.logging.Logger;
+
+import org.kohsuke.stapler.HttpRedirect;
+import org.kohsuke.stapler.HttpResponse;
+import org.kohsuke.stapler.QueryParameter;
+import org.wetator.jenkins.History;
+import org.wetator.jenkins.WetatorBuildReport;
+
+import hudson.Util;
+import hudson.model.AbstractBuild;
+import hudson.model.AbstractModelObject;
+import hudson.model.Run;
+
+/**
+ * The base class for all test results.
+ *
+ * @author frank.danek
+ */
+public abstract class AbstractBaseResult extends AbstractModelObject implements Serializable {
+
+  private static final long serialVersionUID = -139906426226400784L;
+
+  private static final Logger LOG = Logger.getLogger(AbstractBaseResult.class.getName());
+
+  protected transient AbstractBuild<?, ?> owner;
+  protected String name;
+  protected transient AbstractBaseResult parent;
+  protected String fullName;
+  protected long duration;
+
+  @Override
+  public String getDisplayName() {
+    return getName();
+  }
+
+  @Override
+  public String getSearchUrl() {
+    return safe(getName());
+  }
+
+  /**
+   * @return the owning build
+   */
+  public AbstractBuild<?, ?> getOwner() {
+    return owner;
+  }
+
+  /**
+   * @param owner the owning build to set
+   */
+  public void setOwner(AbstractBuild<?, ?> owner) {
+    this.owner = owner;
+  }
+
+  /**
+   * @return the name
+   */
+  public String getName() {
+    return name;
+  }
+
+  /**
+   * @param name the name to set
+   */
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  /**
+   * @return the parent result
+   */
+  public AbstractBaseResult getParent() {
+    return parent;
+  }
+
+  /**
+   * @param parent the parent result to set
+   */
+  public void setParent(AbstractBaseResult parent) {
+    this.parent = parent;
+  }
+
+  /**
+   * @return the full name
+   */
+  public String getFullName() {
+    return fullName;
+  }
+
+  /**
+   * @param fullName the full name to set
+   */
+  public void setFullName(String fullName) {
+    this.fullName = fullName;
+  }
+
+  /**
+   * Replaces URL-unsafe characters.
+   */
+  public static String safe(String s) {
+    // 3 replace calls is still 2-3x faster than a regex replaceAll
+    return s.replace('/', '_').replace('\\', '_').replace(':', '_');
+  }
+
+  /**
+   * @return URL relative to the {@link WetatorBuildReport}
+   */
+  public String getUrl() {
+    return safe(getName());
+  }
+
+  /**
+   * @return the duration
+   */
+  public long getDuration() {
+    return duration;
+  }
+
+  /**
+   * @param aDuration the duration to set
+   */
+  public void setDuration(long aDuration) {
+    duration = aDuration;
+  }
+
+  /**
+   * @return the string representation of the {@link #getDuration()} in a human readable format
+   */
+  public String getDurationString() {
+    return Util.getTimeSpanString(getDuration());
+  }
+
+  /**
+   * Gets the total number of passed tests.
+   */
+  public abstract int getPassCount();
+
+  /**
+   * Gets the total number of skipped tests.
+   */
+  public abstract int getSkipCount();
+
+  /**
+   * Gets the total number of failed tests.
+   */
+  public abstract int getFailCount();
+
+  /**
+   * @return true if the test did not fail, false otherwise.
+   */
+  public boolean isPassed() {
+    return getFailCount() == 0;
+  }
+
+  /**
+   * Tally the results.
+   */
+  public abstract void tally();
+
+  /**
+   * @return the history
+   */
+  public History getHistory() {
+    return new History(this);
+  }
+
+  /**
+   * @return the {@link WetatorBuildReport} that points to the top level test result includes
+   *         this test result
+   */
+  public WetatorBuildReport getParentBuildReport() {
+    return owner.getAction(WetatorBuildReport.class);
+  }
+
+  /**
+   * Gets the counter part of this {@link AbstractBaseResult} in the previous run.
+   *
+   * @return null if no such counter part exists.
+   */
+  public AbstractBaseResult getPreviousResult() {
+    AbstractBuild<?, ?> tmpBuild = getOwner();
+    if (tmpBuild == null) {
+      return null;
+    }
+    while (true) {
+      tmpBuild = tmpBuild.getPreviousBuild();
+      if (tmpBuild == null) {
+        return null;
+      }
+      WetatorBuildReport tmpBuildReport = tmpBuild.getAction(WetatorBuildReport.class);
+      if (tmpBuildReport != null) {
+        AbstractBaseResult tmpResult = tmpBuildReport.findCorrespondingResult(getName());
+        if (tmpResult != null) {
+          return tmpResult;
+        }
+      }
+    }
+  }
+
+  /**
+   * Gets the counter part of this {@link AbstractBaseResult} in the specified run.
+   *
+   * @return null if no such counter part exists.
+   */
+  public AbstractBaseResult getResultInBuild(AbstractBuild<?, ?> build) {
+    WetatorBuildReport tmpBuildReport = build.getAction(WetatorBuildReport.class);
+    if (tmpBuildReport == null) {
+      return null;
+    }
+    return tmpBuildReport.findCorrespondingResult(getName());
+  }
+
+  /**
+   * @return the description (= annotation of the result)
+   */
+  public String getDescription() {
+    WetatorBuildReport tmpReport = getParentBuildReport();
+    if (tmpReport != null) {
+      return tmpReport.getDescription(this);
+    }
+    return "";
+  }
+
+  /**
+   * @param description the description (= annotation of the result) to set
+   */
+  public void setDescription(String description) {
+    // the method parameters must be raw (without leading a) to make stapler work
+    WetatorBuildReport tmpReport = owner.getAction(WetatorBuildReport.class);
+    if (tmpReport != null) {
+      tmpReport.setDescription(this, description);
+    }
+  }
+
+  /**
+   * Used by stabler.
+   *
+   * @param description the description to save
+   * @return the response
+   * @throws IOException in case of problems
+   */
+  public synchronized HttpResponse doSubmitDescription(@QueryParameter String description) throws IOException {
+    // the method parameters must be raw (without leading a) to make stapler work
+    if (getOwner() == null) {
+      LOG.severe("getOwner() is null, can't save description.");
+    } else {
+      getOwner().checkPermission(Run.UPDATE);
+      setDescription(description);
+      getOwner().save();
+    }
+
+    return new HttpRedirect(".");
+  }
+
+  @Override
+  public String toString() {
+    return name;
+  }
+}
