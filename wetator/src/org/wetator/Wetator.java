b1556af7fc21e848734e41fd05d249920b62b596
rbri <rbri@localhost>
2017-12-29 17:11:02 +0000

    % version 1.8.0

---
+++ wetator/src/org/wetator/Wetator.java
@@ -0,0 +1,160 @@
+/*
+ * Copyright (c) 2008-2017 wetator.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package org.wetator;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.swing.JWindow;
+
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.filefilter.WildcardFileFilter;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.wetator.core.IProgressListener;
+import org.wetator.core.WetatorEngine;
+import org.wetator.gui.DialogUtil;
+import org.wetator.progresslistener.StdOutProgressListener;
+import org.wetator.util.Log4jUtil;
+
+/**
+ * The command line interface for the Wetator.
+ *
+ * @author rbri
+ * @author tobwoerk
+ * @author frank.danek
+ */
+public final class Wetator {
+
+  private static final Log LOG = LogFactory.getLog(Wetator.class);
+
+  /**
+   * The start point for the command line call.
+   *
+   * @param anArgsArray the command line arguments
+   */
+  public static void main(final String[] anArgsArray) {
+
+    String tmpConfigFileName = null;
+    File tmpLogFile = null;
+    final List<String> tmpFileNames = new LinkedList<String>();
+    // parse the command line
+    for (int i = 0; i < anArgsArray.length; i++) {
+      final String tmpArg = anArgsArray[i].trim();
+      if ("-log".equals(tmpArg)) {
+        tmpLogFile = new File("wetator.log");
+        Log4jUtil.configureLog(tmpLogFile);
+      } else if ("-p".equals(tmpArg) && i < (anArgsArray.length - 1)) {
+        tmpConfigFileName = anArgsArray[i + 1];
+        i++;
+      } else {
+        tmpFileNames.add(tmpArg);
+      }
+    }
+
+    LOG.info(Version.getFullProductName());
+    LOG.info("    " + com.gargoylesoftware.htmlunit.Version.getProductName() + " "
+        + com.gargoylesoftware.htmlunit.Version.getProductVersion());
+    if (null != tmpLogFile) {
+      LOG.info("    Log file: " + FilenameUtils.normalize(tmpLogFile.getAbsolutePath()));
+    }
+
+    final IProgressListener tmpProgressListener = new StdOutProgressListener();
+
+    try {
+      final WetatorEngine tmpWetatorEngine = new WetatorEngine();
+      try {
+        tmpWetatorEngine.addProgressListener(tmpProgressListener);
+
+        if (null != tmpConfigFileName) {
+          tmpWetatorEngine.setConfigFileName(tmpConfigFileName);
+        }
+        tmpWetatorEngine.init();
+        if (null != tmpLogFile) {
+          tmpWetatorEngine.getConfiguration().enableLog();
+        }
+
+        if (tmpFileNames.isEmpty()) {
+          String tmpPropertyKey = null;
+          final File tmpConfigFile = tmpWetatorEngine.getConfigFile();
+          if (null != tmpConfigFile) {
+            tmpPropertyKey = Integer.toString(tmpConfigFile.getAbsolutePath().hashCode());
+          }
+
+          final JWindow tmpWindow = new JWindow();
+          try {
+            final File[] tmpFiles = DialogUtil.chooseFiles(tmpWindow, tmpPropertyKey);
+            if (null == tmpFiles || tmpFiles.length < 1) {
+              System.exit(0);
+            }
+            for (int i = 0; i < tmpFiles.length; i++) {
+              final File tmpFile = tmpFiles[i];
+              tmpWetatorEngine.addTestCase(tmpFile.getName(), tmpFile);
+            }
+          } finally {
+            tmpWindow.dispose();
+          }
+        } else {
+          final File tmpCurrentDir = new File(".");
+          for (final String tmpFileName : tmpFileNames) {
+            File tmpSearchFile = new File(tmpFileName);
+            if (tmpSearchFile.isAbsolute()) {
+              tmpWetatorEngine.addTestCase(tmpFileName, tmpSearchFile);
+            } else {
+              tmpSearchFile = new File(tmpCurrentDir, tmpFileName);
+              if (tmpSearchFile.exists()) {
+                tmpWetatorEngine.addTestCase(tmpFileName, tmpSearchFile);
+              } else {
+                final File tmpDir = tmpSearchFile.getParentFile();
+                if (tmpDir != null && tmpDir.exists()) {
+                  final FileFilter tmpFilter = new WildcardFileFilter(tmpSearchFile.getName());
+                  final File[] tmpFiles = tmpDir.listFiles(tmpFilter);
+                  if (tmpFiles != null) {
+                    for (int i = 0; i < tmpFiles.length; i++) {
+                      tmpWetatorEngine.addTestCase(tmpFiles[i].getName(), tmpFiles[i]);
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+
+        tmpWetatorEngine.executeTests();
+      } finally {
+        tmpWetatorEngine.shutdown();
+      }
+      // SearchPattern.dumpStatistics();
+    } catch (final Throwable e) {
+      System.out.println("Wetator execution failed: " + e.getMessage());
+      LOG.fatal("Wetator execution failed:", e);
+
+      // System.exit is needed because we have started swing
+      System.exit(1);
+    }
+    System.exit(0);
+  }
+
+  /**
+   * This class should not be instantiated.
+   */
+  private Wetator() {
+    // nothing
+  }
+}
---
+++ wetator/test/java/org/wetator/backend/htmlunit/control/identifier/HtmlUnitSelectIdentifierTest.java
@@ -0,0 +1,369 @@
+/*
+ * Copyright (c) 2008-2017 wetator.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package org.wetator.backend.htmlunit.control.identifier;
+
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.wetator.backend.WPath;
+import org.wetator.backend.WeightedControlList;
+import org.wetator.exception.InvalidInputException;
+import org.wetator.util.SecretString;
+
+/**
+ * @author rbri
+ * @author frank.danek
+ */
+public class HtmlUnitSelectIdentifierTest extends AbstractHtmlUnitControlIdentifierTest {
+
+  @Before
+  public void setupIdentifier() {
+    identifier = new HtmlUnitSelectIdentifier();
+  }
+
+  @Test
+  public void byId() throws IOException, InvalidInputException {
+    // @formatter:off
+    final String tmpHtmlCode = "<html><body>"
+        + "<form action='test'>"
+        + "<select id='MyId' name='MySelectName' size='2'>"
+        + "<option id='MyOptionId' value='o_value1'>option1</option>"
+        + "<option value='o_value2'>option2</option>"
+        + "<option value='o_value3'>option3</option>"
+        + "</select>"
+        + "</form>"
+        + "</body></html>";
+    // @formatter:on
+
+    final SecretString tmpSearch = new SecretString("MyId");
+
+    final WeightedControlList tmpFound = identify(tmpHtmlCode, new WPath(tmpSearch, config), "MyId");
+
+    Assert.assertEquals(1, tmpFound.getEntriesSorted().size());
+    Assert.assertEquals(
+        "[HtmlSelect (id='MyId') (name='MySelectName')] found by: BY_ID deviation: 0 distance: 0 start: 0 index: 5",
+        tmpFound.getEntriesSorted().get(0).toString());
+  }
+
+  @Test
+  public void byName() throws IOException, InvalidInputException {
+    // @formatter:off
+    final String tmpHtmlCode = "<html><body>"
+        + "<form action='test'>"
+        + "FirstSelectLabelText"
+        + "<select id='MyFirstSelectId' name='MyFirstSelectName' size='2'>"
+        + "<option id='1_1' value='o_value1'>option1</option>"
+        + "<option id='1_2' value='o_value2'>option2</option>"
+        + "<option id='1_3' value='o_value3'>option3</option>"
+        + "</select>"
+        + "SecondSelectLabelText"
+        + "<select id='MySecondSelectId' name='MySecondSelectName' size='2'>"
+        + "<option id='2_1' value='o_value1'>option1</option>"
+        + "<option id='2_2' value='o_value2'>option2</option>"
+        + "<option id='2_3' value='o_value3'>option3</option>"
+        + "</select>"
+        + "</form>"
+        + "</body></html>";
+    // @formatter:on
+
+    final SecretString tmpSearch = new SecretString("MyFirstSelectName");
+
+    final WeightedControlList tmpFound = identify(tmpHtmlCode, new WPath(tmpSearch, config), "MyFirstSelectId",
+        "MySecondSelectId");
+
+    Assert.assertEquals(1, tmpFound.getEntriesSorted().size());
+    Assert.assertEquals(
+        "[HtmlSelect (id='MyFirstSelectId') (name='MyFirstSelectName')] found by: BY_NAME deviation: 0 distance: 20 start: 20 index: 6",
+        tmpFound.getEntriesSorted().get(0).toString());
+  }
+
+  @Test
+  public void byLabelingTextBefore() throws IOException, InvalidInputException {
+    // @formatter:off
+    final String tmpHtmlCode = "<html><body>"
+        + "<form action='test'>"
+        + "FirstSelectLabelText"
+        + "<select id='MyFirstSelectId' name='MyFirstSelectName' size='2'>"
+        + "<option id='1_1' value='o_value1'>option1</option>"
+        + "<option id='1_2' value='o_value2'>option2</option>"
+        + "<option id='1_3' value='o_value3'>option3</option>"
+        + "</select>"
+        + "SecondSelectLabelText"
+        + "<select id='MySecondSelectId' name='MySecondSelectName' size='2'>"
+        + "<option id='2_1' value='o_value1'>option1</option>"
+        + "<option id='2_2' value='o_value2'>option2</option>"
+        + "<option id='2_3' value='o_value3'>option3</option>"
+        + "</select>"
+        + "</form>"
+        + "</body></html>";
+    // @formatter:on
+
+    final SecretString tmpSearch = new SecretString("SecondSelectLabelText");
+
+    final WeightedControlList tmpFound = identify(tmpHtmlCode, new WPath(tmpSearch, config), "MyFirstSelectId",
+        "MySecondSelectId");
+
+    Assert.assertEquals(1, tmpFound.getEntriesSorted().size());
+    Assert.assertEquals(
+        "[HtmlSelect (id='MySecondSelectId') (name='MySecondSelectName')] found by: BY_LABELING_TEXT deviation: 0 distance: 45 start: 66 index: 14",
+        tmpFound.getEntriesSorted().get(0).toString());
+  }
+
+  @Test
+  public void byHtmlLabel_Text() throws IOException, InvalidInputException {
+    // @formatter:off
+    final String tmpHtmlCode = "<html><body>"
+        + "<form action='test'>"
+        + "<label id='MyFirstLabelId' for='MyFirstSelectId'>FirstSelectLabelText</label>"
+        + "<select id='MyFirstSelectId' size='2'>"
+        + "<option id='1_1' value='o_value1'>option1</option>"
+        + "<option id='1_2' value='o_value2'>option2</option>"
+        + "<option id='1_3' value='o_value3'>option3</option>"
+        + "</select>"
+        + "<label id='MySecondLabelId' for='MySecondSelectId'>SecondSelectLabelText</label>"
+        + "<select id='MySecondSelectId' size='2'>"
+        + "<option id='2_1' value='o_value1'>option1</option>"
+        + "<option id='2_2' value='o_value2'>option2</option>"
+        + "<option id='2_3' value='o_value3'>option3</option>"
+        + "</select>"
+        + "</form>"
+        + "</body></html>";
+    // @formatter:on
+
+    final SecretString tmpSearch = new SecretString("SecondSelectLabelText");
+
+    final WeightedControlList tmpFound = identify(tmpHtmlCode, new WPath(tmpSearch, config), "MyFirstLabelId",
+        "MySecondLabelId");
+
+    Assert.assertEquals(1, tmpFound.getEntriesSorted().size());
+    Assert.assertEquals(
+        "[HtmlSelect (id='MySecondSelectId')] found by: BY_LABEL deviation: 0 distance: 44 start: 66 index: 16",
+        tmpFound.getEntriesSorted().get(0).toString());
+  }
+
+  @Test
+  public void byHtmlLabel_Text_Invisible() throws IOException, InvalidInputException {
+    // @formatter:off
+    final String tmpHtmlCode = "<html><body>"
+        + "<form action='test'>"
+        + "<label id='MyFirstLabelId' for='MyFirstSelectId'>FirstSelectLabelText</label>"
+        + "<select id='MyFirstSelectId' size='2'>"
+        + "<option id='1_1' value='o_value1'>option1</option>"
+        + "<option id='1_2' value='o_value2'>option2</option>"
+        + "<option id='1_3' value='o_value3'>option3</option>"
+        + "</select>"
+        + "<label id='MySecondLabelId' for='MySecondSelectId'>SecondSelectLabelText</label>"
+        + "<select id='MySecondSelectId' size='2' style='display: none;'>"
+        + "<option id='2_1' value='o_value1'>option1</option>"
+        + "<option id='2_2' value='o_value2'>option2</option>"
+        + "<option id='2_3' value='o_value3'>option3</option>"
+        + "</select>"
+        + "</form>"
+        + "</body></html>";
+    // @formatter:on
+
+    final SecretString tmpSearch = new SecretString("SecondSelectLabelText");
+
+    final WeightedControlList tmpFound = identify(tmpHtmlCode, new WPath(tmpSearch, config), "MyFirstLabelId",
+        "MySecondLabelId");
+
+    Assert.assertEquals(0, tmpFound.getEntriesSorted().size());
+  }
+
+  @Test
+  public void byHtmlLabelChild_Text() throws IOException, InvalidInputException {
+    // @formatter:off
+    final String tmpHtmlCode = "<html><body>"
+        + "<form action='test'>"
+        + "<label id='MyFirstLabelId'>FirstSelectLabelText"
+        + "<select id='MyFirstSelectId' size='2'>"
+        + "<option id='1_1' value='o_value1'>option1</option>"
+        + "<option id='1_2' value='o_value2'>option2</option>"
+        + "<option id='1_3' value='o_value3'>option3</option>"
+        + "</select>"
+        + "</label>"
+        + "<label id='MySecondLabelId'>SecondSelectLabelText"
+        + "<select id='MySecondSelectId' size='2'>"
+        + "<option id='2_1' value='o_value1'>option1</option>"
+        + "<option id='2_2' value='o_value2'>option2</option>"
+        + "<option id='2_3' value='o_value3'>option3</option>"
+        + "</select>"
+        + "</label>"
+        + "</form>"
+        + "</body></html>";
+    // @formatter:on
+
+    final SecretString tmpSearch = new SecretString("SecondSelectLabelText");
+
+    final WeightedControlList tmpFound = identify(tmpHtmlCode, new WPath(tmpSearch, config), "MyFirstLabelId",
+        "MySecondLabelId");
+
+    Assert.assertEquals(1, tmpFound.getEntriesSorted().size());
+    Assert.assertEquals(
+        "[HtmlSelect (id='MySecondSelectId')] found by: BY_LABEL deviation: 0 distance: 44 start: 66 index: 16",
+        tmpFound.getEntriesSorted().get(0).toString());
+  }
+
+  @Test
+  public void byHtmlLabelChild_Text_Invisible() throws IOException, InvalidInputException {
+    // @formatter:off
+    final String tmpHtmlCode = "<html><body>"
+        + "<form action='test'>"
+        + "<label id='MyFirstLabelId'>FirstSelectLabelText"
+        + "<select id='MyFirstSelectId' size='2'>"
+        + "<option id='1_1' value='o_value1'>option1</option>"
+        + "<option id='1_2' value='o_value2'>option2</option>"
+        + "<option id='1_3' value='o_value3'>option3</option>"
+        + "</select>"
+        + "</label>"
+        + "<label id='MySecondLabelId'>SecondSelectLabelText"
+        + "<select id='MySecondSelectId' size='2' style='display: none;'>"
+        + "<option id='2_1' value='o_value1'>option1</option>"
+        + "<option id='2_2' value='o_value2'>option2</option>"
+        + "<option id='2_3' value='o_value3'>option3</option>"
+        + "</select>"
+        + "</label>"
+        + "</form>"
+        + "</body></html>";
+    // @formatter:on
+
+    final SecretString tmpSearch = new SecretString("SecondSelectLabelText");
+
+    final WeightedControlList tmpFound = identify(tmpHtmlCode, new WPath(tmpSearch, config), "MyFirstLabelId",
+        "MySecondLabelId");
+
+    Assert.assertEquals(0, tmpFound.getEntriesSorted().size());
+  }
+
+  @Test
+  public void byWholeTextBefore() throws IOException, InvalidInputException {
+    // @formatter:off
+    final String tmpHtmlCode = "<html><body>"
+        + "<form action='test'>"
+        + "<p>Marker</p>"
+        + "<input id='otherId' name='otherName' type='submit'>"
+        + "<p>Some text ...</p>"
+        + "<select id='myId' name='myName' size='2'>"
+        + "<option id='2_1' value='o_value1'>option1</option>"
+        + "<option id='2_2' value='o_value2'>option2</option>"
+        + "<option id='2_3' value='o_value3'>option3</option>"
+        + "</select>"
+        + "</form>"
+        + "</body></html>";
+    // @formatter:on
+
+    final SecretString tmpSearch = new SecretString("Marker");
+
+    final WeightedControlList tmpFound = identify(tmpHtmlCode, new WPath(tmpSearch, config), "myId");
+
+    Assert.assertEquals(1, tmpFound.getEntriesSorted().size());
+    Assert.assertEquals(
+        "[HtmlSelect (id='myId') (name='myName')] found by: BY_TEXT deviation: 14 distance: 20 start: 20 index: 10",
+        tmpFound.getEntriesSorted().get(0).toString());
+  }
+
+  @Test
+  public void byWholeTextBefore_wildcardOnly() throws IOException, InvalidInputException {
+    // @formatter:off
+    final String tmpHtmlCode = "<html><body>"
+        + "<form action='test'>"
+        + "<p>Marker</p>"
+        + "<select id='myId' name='myName' size='2'>"
+        + "<option id='1_1' value='o_value1'>option1</option>"
+        + "<option id='1_2' value='o_value2'>option2</option>"
+        + "<option id='1_3' value='o_value3'>option3</option>"
+        + "</select>"
+        + "<p>Some text ...</p>"
+        + "<select id='otherId' name='otherName' size='2'>"
+        + "<option id='2_1' value='o_value1'>option1</option>"
+        + "<option id='2_2' value='o_value2'>option2</option>"
+        + "<option id='2_3' value='o_value3'>option3</option>"
+        + "</select>"
+        + "</form>"
+        + "</body></html>";
+    // @formatter:on
+
+    final SecretString tmpSearch = new SecretString("Marker > ");
+
+    final WeightedControlList tmpFound = identify(tmpHtmlCode, new WPath(tmpSearch, config), "myId", "otherId");
+
+    Assert.assertEquals(2, tmpFound.getEntriesSorted().size());
+    Assert.assertEquals(
+        "[HtmlSelect (id='myId') (name='myName')] found by: BY_TEXT deviation: 0 distance: 0 start: 6 index: 7",
+        tmpFound.getEntriesSorted().get(0).toString());
+    Assert.assertEquals(
+        "[HtmlSelect (id='otherId') (name='otherName')] found by: BY_TEXT deviation: 0 distance: 38 start: 44 index: 16",
+        tmpFound.getEntriesSorted().get(1).toString());
+  }
+
+  @Test
+  public void byTableCoordinates() throws IOException, InvalidInputException {
+    // @formatter:off
+    final String tmpHtmlCode = "<html><body>"
+        + "    <table border='0' cellspacing='20' cellpadding='30'>"
+        + "      <thead>"
+        + "        <tr>"
+        + "          <th id='header_1'>header_1</th>"
+        + "          <th id='header_2'>header_2</th>"
+        + "          <th id='header_3'>header_3</th>"
+        + "        </tr>"
+        + "      </thead>"
+        + "      <tbody>"
+        + "        <tr>"
+        + "          <td id='cell_1_1'>row_1</td>"
+        + "          <td id='cell_1_2'><select id='myId_1_2' size='2'>"
+        + "            <option id='MyOptionId' value='o_value1'>option1</option>"
+        + "            <option value='o_value2'>option2</option>"
+        + "            <option value='o_value3'>option3</option>"
+        + "          </select></td>"
+        + "          <td id='cell_1_3'><select id='myId_1_3' size='2'>"
+        + "            <option id='MyOptionId' value='o_value1'>option1</option>"
+        + "            <option value='o_value2'>option2</option>"
+        + "            <option value='o_value3'>option3</option>"
+        + "          </select></td>"
+        + "        </tr>"
+        + "        <tr>"
+        + "          <td id='cell_2_1'>row_2</td>"
+        + "          <td id='cell_2_2'><select id='myId_2_2' size='2'>"
+        + "            <option id='MyOptionId' value='o_value1'>option1</option>"
+        + "            <option value='o_value2'>option2</option>"
+        + "            <option value='o_value3'>option3</option>"
+        + "          </select></td>"
+        + "          <td id='cell_2_3'><select id='myId_2_3' size='2'>"
+        + "            <option id='MyOptionId' value='o_value1'>option1</option>"
+        + "            <option value='o_value2'>option2</option>"
+        + "            <option value='o_value3'>option3</option>"
+        + "          </select></td>"
+        + "        </tr>"
+        + "      </tbody>"
+        + "    </table>"
+        + "</body></html>";
+    // @formatter:on
+
+    final SecretString tmpSearch = new SecretString("[header_3; row_2]");
+
+    final WeightedControlList tmpFound = identify(tmpHtmlCode, new WPath(tmpSearch, config), "myId_1_2", "myId_1_3",
+        "myId_2_2", "myId_2_3");
+
+    Assert.assertEquals(1, tmpFound.getEntriesSorted().size());
+    Assert.assertEquals(
+        "[HtmlSelect (id='myId_2_3')] found by: BY_TABLE_COORDINATE deviation: 0 distance: 110 start: 110 index: 63",
+        tmpFound.getEntriesSorted().get(0).toString());
+  }
+}
