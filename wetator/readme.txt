f8036991a011985cd4a27ec21b362203142ff67b
rbri <rbri@localhost>
2021-04-11 15:04:07 +0000

    version 1.13.0

---
+++ wetator/readme.txt
@@ -0,0 +1,72 @@
+HOW TO SETUP THE IDE
+====================
+Currently we use
+    Eclipse 2020-06 (4.16.0)
+    Eclipse Checkstyle Plugin 8.35.0 (based on Checkstyle 8.35)
+as IDE. All other settings (code format, warnings, etc.) are stored with the project.
+
+
+HOW TO SETUP THE PROJECT
+========================
+Run
+    ant resolve-dependencies
+to retrieve all needed libraries (and their sources if available) into the folder /lib (and /lib_src).
+
+!IMPORTANT! NEVER check in the folders /build and /deploy or the content of the folders /lib and /lib_src.
+
+
+HOW TO BUILD THE PROJECT
+========================
+Just run
+    ant package
+and everything will be done and placed into the folder /deploy.
+
+
+HOW TO ADD LIBRARIES
+====================
+Just add the needed libraries as new dependencies to the file
+    ivy.xml
+(see IVY documentation for more details) and run
+    ant resolve-dependencies
+to retrieve the new libraries into the folder /lib. After that you can just add them to the classpath.
+
+
+HOW TO RELEASE
+==============
+
+WETATOR
+  * force jenkins build and check for success
+  * adjust version info in file build-properties.xml and commit
+  * create svn tag
+    - create folder tags/version_x_x_xx
+    - copy trunk/wetator to the new folder
+  * build the release
+    - provide your credential as ant call parameters -Dsonatype.username=???? -Dsonatype.passwd=???? -Dpgp.passphrase=???? -v
+    - ant clean
+    - ant publish-sonatype-releases
+  * test the release
+    - ant test-release
+  * go to https://oss.sonatype.org/index.html#stagingRepositories
+      close the repository
+      release the repository
+
+WETATOR-ANT
+  * change ivy.xml to point to the latest wetator release
+  * force resolve-dependencies and adjust classpath of the eclipse project and commit
+  * force jenkins build and check for success
+  * adjust version info in file build-properties.xml and commit
+  * create svn tag
+    - copy trunk/wetator-ant-task to the new folder
+  * build the release
+    - ant clean
+    - ant publish-sonatype-releases
+  * test the release
+    - ant test-release
+  * go to https://oss.sonatype.org/index.html#stagingRepositories
+      close the repository
+      release the repository
+
+  * update website
+    - download page
+    - release notes
+    - news
---
+++ wetator/test/java/org/wetator/commandset/XmlEventHandlingTest.java
@@ -0,0 +1,142 @@
+/*
+ * Copyright (c) 2008-2021 wetator.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package org.wetator.commandset;
+
+import java.io.File;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.wetator.backend.IBrowser.BrowserType;
+import org.wetator.exception.InvalidInputException;
+import org.wetator.test.AbstractWebServerTest;
+import org.wetator.test.junit.BrowserRunner;
+import org.wetator.test.junit.BrowserRunner.Browsers;
+
+/**
+ * @author frank.danek
+ * @author rbri
+ */
+@RunWith(BrowserRunner.class)
+public class XmlEventHandlingTest extends AbstractWebServerTest {
+
+  private static final String BASE_FOLDER = "test/xml/";
+
+  @Test
+  @Browsers({ BrowserType.FIREFOX_78 })
+  public void eventClickOnFF68() throws InvalidInputException {
+    executeTestFile("ff60/event_clickOn.wet");
+
+    Assert.assertEquals(63, getSteps());
+    Assert.assertEquals(0, getFailures());
+    Assert.assertEquals(0, getErrors());
+  }
+
+  @Test
+  @Browsers({ BrowserType.INTERNET_EXPLORER })
+  public void eventClickOnIE() throws InvalidInputException {
+    executeTestFile("ie11/event_clickOn.wet");
+
+    Assert.assertEquals(63, getSteps());
+    Assert.assertEquals(0, getFailures());
+    Assert.assertEquals(0, getErrors());
+  }
+
+  @Test
+  @Browsers({ BrowserType.FIREFOX_78 })
+  public void eventDeselectFF68() throws InvalidInputException {
+    executeTestFile("ff60/event_deselect.wet");
+
+    Assert.assertEquals(18, getSteps());
+    Assert.assertEquals(0, getFailures());
+    Assert.assertEquals(0, getErrors());
+  }
+
+  @Test
+  @Browsers({ BrowserType.INTERNET_EXPLORER })
+  public void eventDeselectIE1() throws InvalidInputException {
+    executeTestFile("ie11/event_deselect.wet");
+
+    Assert.assertEquals(18, getSteps());
+    Assert.assertEquals(0, getFailures());
+    Assert.assertEquals(0, getErrors());
+  }
+
+  @Test
+  @Browsers({ BrowserType.FIREFOX_78 })
+  public void eventMouseOverFF68() throws InvalidInputException {
+    executeTestFile("ff60/event_mouseOver.wet");
+
+    Assert.assertEquals(74, getSteps());
+    Assert.assertEquals(0, getFailures());
+    Assert.assertEquals(0, getErrors());
+  }
+
+  @Test
+  @Browsers({ BrowserType.INTERNET_EXPLORER })
+  public void eventMouseOverIE11() throws InvalidInputException {
+    executeTestFile("ie11/event_mouseOver.wet");
+
+    Assert.assertEquals(74, getSteps());
+    Assert.assertEquals(0, getFailures());
+    Assert.assertEquals(0, getErrors());
+  }
+
+  @Test
+  @Browsers({ BrowserType.FIREFOX_78 })
+  public void eventSelectFF68() throws InvalidInputException {
+    executeTestFile("ff60/event_select.wet");
+
+    Assert.assertEquals(18, getSteps());
+    Assert.assertEquals(0, getFailures());
+    Assert.assertEquals(0, getErrors());
+  }
+
+  @Test
+  @Browsers({ BrowserType.INTERNET_EXPLORER })
+  public void eventSelectIE11() throws InvalidInputException {
+    executeTestFile("ie11/event_select.wet");
+
+    Assert.assertEquals(18, getSteps());
+    Assert.assertEquals(0, getFailures());
+    Assert.assertEquals(0, getErrors());
+  }
+
+  @Test
+  @Browsers({ BrowserType.FIREFOX_78 })
+  public void eventSetFF68() throws InvalidInputException {
+    executeTestFile("ff60/event_set.wet");
+
+    Assert.assertEquals(27, getSteps());
+    Assert.assertEquals(0, getFailures());
+    Assert.assertEquals(0, getErrors());
+  }
+
+  @Test
+  @Browsers({ BrowserType.INTERNET_EXPLORER })
+  public void eventSetIE11() throws InvalidInputException {
+    executeTestFile("ie11/event_set.wet");
+
+    Assert.assertEquals(27, getSteps());
+    Assert.assertEquals(0, getFailures());
+    Assert.assertEquals(0, getErrors());
+  }
+
+  private void executeTestFile(final String aTestFileName) throws InvalidInputException {
+    executeTestFile(new File(BASE_FOLDER + aTestFileName));
+  }
+}
